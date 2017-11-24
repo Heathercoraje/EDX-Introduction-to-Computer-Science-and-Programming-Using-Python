4697983ad755cea747496763900384df312a6b4e
heathercoraje <yhk7830@hotmail.com>
2017-11-24 13:25:50 +0700

    add solution to problem1

---
+++ week3/problem/problem1.py
@@ -0,0 +1,21 @@
+# problem 1
+# Please read the Hangman Introduction before starting this problem. We'll start by writing 3 simple functions that will help us easily code the Hangman problem. First, implement the function isWordGuessed that takes in two parameters - a string, secretWord, and a list of letters, lettersGuessed. This function returns a boolean - True if secretWord has been guessed (ie, all the letters of secretWord are in lettersGuessed) and False otherwise.
+#
+
+def isWordGuessed(secretWord, lettersGuessed):
+    '''
+    secretWord: string, the word the user is guessing
+    lettersGuessed: list, what letters have been guessed so far
+    returns: boolean, True if all the letters of secretWord are in lettersGuessed;
+      False otherwise
+    '''
+    secretList = list(secretWord)
+    i = 0
+
+    while (i < len(secretList)):
+        if (secretList[i] in lettersGuessed): # is True
+            i += 1
+        else:
+            return False
+            break
+    return True
