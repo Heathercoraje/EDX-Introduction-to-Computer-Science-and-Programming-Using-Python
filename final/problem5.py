ffaf94a84397e390198e69e45404907e68094b48
heathercoraje <yhk7830@hotmail.com>
2017-12-23 22:27:04 +0900

    fix bug in problem5

--- final/problem5.py
+++ final/problem5.py
@@ -1,15 +1,19 @@
-aDict = {1: 1, 3: 2, 6: 0, 7: 0, 8: 4, 10: 0}
+#aDict = {1: 3, 3: 4, 6: 5, 7: 3, 8: 2, 10: 0}
+#aDict = {1: 1, 3: 2, 6: 0, 7: 0, 8: 4, 10: 0}
+
 def uniqueValues(aDict):
     '''
     aDict: a dictionary
     returns: a sorted list of keys that map to unique aDict values, empty list if none
     '''
     if (not aDict): #if an empty dict is given, return False (non-empty dict is True)
-        return {}
+        return []
     else:
         result = [] # container
         values = list(aDict.values()) # extract values from aDict and remove replicates
+        print('values: ', values)
         uniqueValues = [value for value in values if values.count(value) == 1]
+        print('uniqueValues: ', uniqueValues)
         for uniqueValue in uniqueValues:
             for key in aDict.keys():
                 if aDict[key] == uniqueValue:
@@ -17,5 +21,6 @@ def uniqueValues(aDict):
                     break # move on to next value to find key
         result.sort()
         return result
-        
+
 # 19.09 out of 20, I am not covering all cases
+print(uniqueValues(aDict), 'expected: []')
